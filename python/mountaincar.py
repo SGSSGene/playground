6b2be8d0b109cdf370069e7990fb9f28b46471fb
Daniel Goehring <daniel.goehring@gmail.com>
2026-09-11 14:26:30 +0200

    Update mountaincar.py
    cleaned up, added configuration parameters

--- python/mountaincar.py
+++ python/mountaincar.py
@@ -2,6 +2,7 @@
 """
 This is a manual mountain car(t) with physics, exit by pressing ESC
 Created 2026 by Daniel Goehring
+Todo: Non-deterministic physics, e.g., gaussian, multi agents
 """
 import math
 import sys
@@ -11,13 +12,16 @@ import pygame
 WINDOW_SIZE = (1024, 1024)
 BG_COLOR = (30, 30, 30)
 MAX_IMAGE_SIZE = 1024  # maximum width/height to scale the image to
+MOUNTAIN_HEIGHT = 256
 HORIZONTAL_SCALE_FACTOR = 0.007
+MAX_STEPS_PER_EPISODE = 2000
 
 def mountain_height_and_derivative(x):
-    return 256 * math.cos(HORIZONTAL_SCALE_FACTOR*x) + 256, -256 * math.sin(HORIZONTAL_SCALE_FACTOR*x)
+    return MOUNTAIN_HEIGHT * math.cos(HORIZONTAL_SCALE_FACTOR*x), -MOUNTAIN_HEIGHT * math.sin(HORIZONTAL_SCALE_FACTOR*x)
 
 def update_state(p, v, action, steps):
     #this calculates the height and ascent on the mountain
+    trunc_or_term = 0
     y, d = mountain_height_and_derivative(p)
 
     # action leads to increased / decreased velocity
@@ -37,25 +41,35 @@ def update_state(p, v, action, steps):
 
     if p > MAX_IMAGE_SIZE:
         reward = 100
-        print("initialize new episode 001")
+        print("SUCCESS, resetting 01")
 
         p = math.pi / HORIZONTAL_SCALE_FACTOR  #start at center
         v = 0 #positive reward
         steps = 0
+        trunc_or_term = 1
 
 
     elif p <= 0:
         reward = -100
-        print("initialize new episode 002")
+        print("FAIL TOO FAR LEFT, resetting 02")
         p = math.pi / HORIZONTAL_SCALE_FACTOR
         v = 0 #negative reward
         steps = 0
+        trunc_or_term = 1
 
     else:
+        if steps > MAX_STEPS_PER_EPISODE:
+            print("TOO MANY STEPS, resetting 03")
+            p = math.pi / HORIZONTAL_SCALE_FACTOR
+            v = 0
+            steps = 0
+            trunc_or_term = 2
         reward = -1
 
+
+
     # needs to add value
-    return p, v, steps, reward
+    return p, v, steps, reward, trunc_or_term
 
 def agent_request(p,v,steps):
 
@@ -64,9 +78,9 @@ def agent_request(p,v,steps):
     # 1 right
 
     #here comes the state transition model, simple physics
-    p, v, steps, reward = update_state(p, v, action,steps)
+    p, v, steps, reward, trunc_or_term = update_state(p, v, action,steps)
 
-    return p, v, steps
+    return p, v, steps, trunc_or_term
 
 def main():
     pygame.init()
@@ -82,14 +96,14 @@ def main():
     #start velocity
     v = 0
 
-    #episodes = 0
-    i = 0
+    episodes = 0
+    steps = 0
     while running:
         #SWITCH THIS ONE OFF FOR FAST SIMULATION or ON for real physics
         dt = clock.tick(100) / 1000.0  # seconds elapsed since last frame
-        i = i + 1
-        if i % 100 == 0:
-            print (i)
+        steps = steps + 1
+        if steps % 100 == 0:
+            print ("Episode: ", episodes, " Step: ", steps)
 
         #for manual intervention
         for event in pygame.event.get():
@@ -99,22 +113,25 @@ def main():
                 running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
                 v = v - 8
-                print(v)
+                print("Pressed Left")
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
                 v = v + 8
+                print("Pressed Right")
+        p, v, steps, trunc_or_term = agent_request(p, v, steps)
 
-        p, v, i = agent_request(p, v, i)
+        if trunc_or_term !=0:
+            episodes += 1
 
         #draw mountain and car
         screen.fill(BG_COLOR)
         #draw at first the mountain
         for x in range(1024):
             height, derivative = mountain_height_and_derivative(x)
-            pygame.draw.circle(screen, (240, 240, 0), (x + 10, WINDOW_SIZE[1] - height-128), 1)
+            pygame.draw.circle(screen, (240, 240, 0), (x + 10, WINDOW_SIZE[1]/2 - height), 1)
 
         #now draw the car
         height, derivative = mountain_height_and_derivative(p)
-        pygame.draw.circle(screen, (240, 240, 240), (p + 10, WINDOW_SIZE[1] - height-140), 10)
+        pygame.draw.circle(screen, (240, 240, 240), (p + 10, WINDOW_SIZE[1]/2 - height-12), 10)
 
         pygame.display.flip()
 
